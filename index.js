943356cb37e7b9c7931abe65ff05dfbbbe52708f
Rahul <chrahulofficial@gmail.com>
2023-06-03 13:40:39 +0530

    Rahul's Commit

--- index.js
+++ index.js
@@ -1,52 +1,45 @@
-if('webkitSpeechRecognition' in window){
+if ("webkitSpeechRecognition" in window) {
+  const recognition = new webkitSpeechRecognition();
 
-    const recognition = new webkitSpeechRecognition();
+  // config. options......
+  recognition.continuous = true;
+  recognition.interimResults = true; //not the final result
+  recognition.lang = "en-US";
 
-    // config. options......
-    recognition.continuous     = true;
-    recognition.interimResults = true;  //not the final result
-    recognition.lang = 'en-US';
-    
+  const start = document.getElementById("start");
+  const content = document.getElementById("textfield");
+  const end = document.getElementById("end");
 
-    const start = document.getElementById("start");
-    const content = document.getElementById("textfield");
-    const end = document.getElementById("end");
+  let finalScript = "";
 
-    let finalScript = '';
-
-    recognition.onstart = () => {
-        start.innerHTML = 'GPT is Listening.....';
+  recognition.onstart = () => {
+    start.style.display = "none";
+  };
+  // result ......
+  recognition.onresult = (event) => {
+    let interimScript = "";
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      // if result is final ......
+      if (event.results[i].isFinal) {
+        finalScript += event.results[i][0].transcript;
+      } else {
+        interimScript += event.results[i][0].transcript;
+      }
     }
-
-    // result ......
-    recognition.onresult = (event) => {
-        let interimScript = '';
-        
-        for(let i = event.resultIndex; i < event.results.length; ++i){
-        // if result is final ......
-        if(event.results[i].isFinal){
-            finalScript += event.results[i][0].transcript;
-        }
-        else{
-            interimScript += event.results[i][0].transcript;
-        }
-
-        }
-        content.innerHTML = finalScript + '<i style = "color: gray;"' + interimScript + '</i>';
-    }
-
-    // start btn ......
-   start.onclick = () => {
+    content.innerHTML = finalScript;
+  };
+  // start btn ......
+  start.onclick = () => {
     recognition.start();
-   }
+    end.classList.remove("hidden");
+  };
 
-   end.onclick = () => {
+  end.onclick = () => {
+    end.style.display = "none";
+    start.style.display = "block";
     recognition.stop();
-    start.innerHTML = 'Start';
-   }
-
-   alert("is on");
-}
-else {
-    alert("Sorry, the extension is not supported in your browser...");
+    alert(content.innerHTML);
+  };
+} else {
+  alert("Sorry, the extension is not supported in your browser...");
 }
